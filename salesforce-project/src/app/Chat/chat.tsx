c9032b11c4fc1ce7e47b8e785ad8f7819c6cab16
Fabiola Neris <fabiolafalcao.n@gmail.com>
2024-05-15 15:30:37 -0300

    formulario-botao

--- salesforce-project/src/app/Chat/chat.tsx
+++ salesforce-project/src/app/Chat/chat.tsx
@@ -1,7 +1,44 @@
+'use client'
+import { useState } from "react";
+
+
 const Chat =()=>{
+
+    const [showForm, setShowForm] = useState(false);
+
+    const handleButtonClick = () =>{
+        setShowForm(true);
+    };
+
+    const handleCloseForm = ()=>{
+        setShowForm(false);
+    }
+
+
     return(
         <>
-        <button className="chat-button">Saiba Mais</button>
+        <button className="chat-button" onClick={handleButtonClick}>Saiba Mais</button>
+
+        {showForm&& (
+            <div className="box-form">
+                <h2>Preencha seus dados</h2>
+                <form onSubmit={(e) => e.preventDefault()}>
+                    <label htmlFor="companyName">Nome da Empresa</label>
+                    <input type="text" id="companyName" name="companyName" required />
+
+                    <label htmlFor="cnpj">CNPJ:</label>
+                    <input type="text" id="cnpj" name="cnpj" required />
+
+                    <label htmlFor="email">Email de Contato Corporativo:</label>
+                    <input type="email" id="email" name="email" required />
+
+                    <button type="submit" onClick={handleCloseForm}>Enviar</button>
+
+                </form>
+
+            </div>
+
+        )}
         </>
     )
 }
